d99d945c5fe88e2fa204c497b5beeb86699095c8
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-03-23 16:22:53 -0700

    Update build.zig

--- build.zig
+++ build.zig
@@ -1,69 +1,103 @@
 const std = @import("std");
 const Target = std.Target;
+const Feature = @import("std").Target.Cpu.Feature;
+const kernel_name = "kiwiRTOS.elf";
 
 // write a build.zig file that changes the bootloader for the operating system depending on the architecture specified via the command line argument target_arch
 
 pub fn build(b: *std.Build) anyerror!void {
     const optimize = b.standardOptimizeOption(.{});
-
     // Define the supported target architectures
-    const TargetArch = enum { riscv32, riscv64, arm };
+    const TargetArch = enum { riscv32, riscv64, arm, x86 };
 
     // Parse the target architecture from command line or use a default
-    const target_arch_str = b.option([]const u8, "target_arch", "The target architecture to build for (riscv32, riscv64, or arm)") orelse @panic("Target CPU architecture must be specified");
+    const target_arch_str = b.option([]const u8, "target_arch", "The target architecture to build for (riscv32, riscv64, or arm, x86)") orelse @panic("Target CPU architecture must be specified");
     const target_arch = std.meta.stringToEnum(TargetArch, target_arch_str) orelse {
-        std.debug.print("Error: Invalid target architecture '{s}'. Supported architectures are: riscv32, riscv64, arm\n", .{target_arch_str});
+        std.debug.print("Error: Invalid target architecture '{s}'. Supported architectures are: riscv32, riscv64, arm, x86\n", .{target_arch_str});
         return error.InvalidTargetArch;
     };
 
+    var disabled_features = Feature.Set.empty;
+    var enabled_features = Feature.Set.empty;
+
+    switch (target_arch) {
+        .riscv32, .riscv64 => {
+            // enable Multiply extension
+            enabled_features.addFeature(@intFromEnum(Target.riscv.Feature.m));
+            // disable all CPU extensions
+            disabled_features.addFeature(@intFromEnum(Target.riscv.Feature.a));
+            disabled_features.addFeature(@intFromEnum(Target.riscv.Feature.c));
+            disabled_features.addFeature(@intFromEnum(Target.riscv.Feature.d));
+            disabled_features.addFeature(@intFromEnum(Target.riscv.Feature.e));
+            disabled_features.addFeature(@intFromEnum(Target.riscv.Feature.f));
+        },
+        .arm => {},
+        .x86 => {},
+    }
+
     // Define the target architecture that the kernel will be built for
     const target = switch (target_arch) {
-        .riscv32 => b.standardTargetOptions(.{ .default_target = .{
-            .cpu_arch = .riscv32,
-            .os_tag = .freestanding,
-            .abi = .none,
-        }}),
-        .riscv64 => b.standardTargetOptions(.{
-            .default_target = .{
-                .cpu_arch = .riscv64,
-                .os_tag = .freestanding,
-                .abi = .none,
-            }
-        }),
-        .arm => b.standardTargetOptions(.{
-            .default_target = .{
-                .cpu_arch = .aarch64,
-                .os_tag = .freestanding,
-                .abi = .none,
-            }
-        }),
+        .riscv32 => b.resolveTargetQuery((.{
+            .cpu_arch = Target.Cpu.Arch.riscv32,
+            .os_tag = Target.Os.Tag.freestanding,
+            .abi = Target.Abi.none,
+            .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
+            .cpu_features_sub = disabled_features,
+            .cpu_features_add = enabled_features,
+        })),
+        .riscv64 => b.resolveTargetQuery((.{
+            .cpu_arch = Target.Cpu.Arch.riscv64,
+            .os_tag = Target.Os.Tag.freestanding,
+            .abi = Target.Abi.none,
+            .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv64 },
+            .cpu_features_sub = disabled_features,
+            .cpu_features_add = enabled_features,
+        })),
+        .arm => b.resolveTargetQuery((.{
+            .cpu_arch = Target.Cpu.Arch.aarch64,
+            .os_tag = Target.Os.Tag.freestanding,
+            .abi = Target.Abi.none,
+            .cpu_model = .{ .explicit = &std.Target.aarch64.cpu.generic },
+            .cpu_features_sub = disabled_features,
+            .cpu_features_add = enabled_features,
+        })),
+        .x86 => b.resolveTargetQuery((.{
+            .cpu_arch = Target.Cpu.Arch.x86,
+            .os_tag = Target.Os.Tag.freestanding,
+            .abi = Target.Abi.none,
+            .cpu_model = .{ .explicit = &std.Target.x86.cpu.generic },
+            .cpu_features_sub = disabled_features,
+            .cpu_features_add = enabled_features,
+        })),
     };
 
     const kernel = b.addExecutable(.{
         .root_source_file = b.path("src/main.zig"),
         .optimize = optimize,
         .target = target,
-        .name = "kiwiRTOS.elf",
-        .code_model = .medium,
+        .name = kernel_name,
+        .code_model = switch (target_arch) {
+            .riscv64, .riscv32 => .medium,
+            else => .kernel,
+        },
     });
 
     // Set the appropriate linker script based on the target architecture
-    const linker_script = switch (target.result.cpu.arch) {
+    const linker_script = switch (target_arch) {
         .riscv32 => "src/arch/riscv/32/linker.lds",
         .riscv64 => "src/arch/riscv/64/linker.lds",
         .arm => "src/arch/arm/linker.lds",
-        .x86_64 => "src/arch/x86_64/linker.lds",
-        else => unreachable,
+        .x86 => "src/arch/x86/linker.lds",
     };
 
     kernel.setLinkerScriptPath(b.path(linker_script));
 
     // Set the boot-code depending on the target architecture
-    const boot_code = switch (target.result.cpu.arch) {
+    const boot_code = switch (target_arch) {
         .riscv32 => "src/arch/riscv/32/boot.S",
         .riscv64 => "src/arch/riscv/64/boot.S",
         .arm => "src/arch/arm/boot.S",
-        else => unreachable,
+        .x86 => "src/arch/x86/boot.S",
     };
 
     kernel.addCSourceFiles(.{
@@ -74,29 +108,29 @@ pub fn build(b: *std.Build) anyerror!void {
     });
     b.installArtifact(kernel);
 
-    const qemu = switch (target.result.cpu.arch) {
+    const qemu = switch (target_arch) {
         .riscv64 => "qemu-system-riscv64",
         .riscv32 => "qemu-system-riscv32",
         .arm => "qemu-system-aarch64",
-        else => unreachable,
+        .x86 => "qemu-system-x86",
     };
 
-    const qemu_cpu = switch (target.result.cpu.arch) {
+    const qemu_cpu = switch (target_arch) {
         .riscv64 => "rv64",
         .riscv32 => "rv32",
         .arm => "cortex-a53",
-        else => unreachable,
+        .x86 => "qemu-x86",
     };
 
     const qemu_cmd = b.addSystemCommand(&.{
-        qemu,                 "-machine",
-        "virt",               "-bios",
-        "none",               "-kernel",
-        "zig-out/bin/kernel", "-m",
-        "128M",               "-cpu",
-        qemu_cpu,             "-smp",
-        "4",                  "-nographic",
-        "-serial",            "mon:stdio",
+        qemu,                          "-machine",
+        "virt",                        "-bios",
+        "none",                        "-kernel",
+        "zig-out/bin/" ++ kernel_name, "-m",
+        "128M",                        "-cpu",
+        qemu_cpu,                      "-smp",
+        "4",                           "-nographic",
+        "-serial",                     "mon:stdio",
     });
 
     qemu_cmd.step.dependOn(b.getInstallStep());
